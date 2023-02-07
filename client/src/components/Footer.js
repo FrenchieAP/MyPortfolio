7ca2e78349607f20a6d97c0293350fee9952e669
Antoine Pepino <111791015+FrenchieAP@users.noreply.github.com>
2023-02-07 14:54:12 -0800

    Add files via upload

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = (props) => {
+    return (
+        <div>
+            <div className="foot">
+                <h3><a href="/" className="navtab">Back to top</a></h3>
+                <p className="copy">Copyright Antoine Pepino 2023</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
